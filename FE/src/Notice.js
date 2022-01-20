20c7a004de695af4f39ef6eb30c2f9066f0c687c
이종준 <ljj9316@naver.com>
2022-01-20 15:09:24 +0900

    Feat: Make Notice structure

--- FE/src/Notice.js
+++ FE/src/Notice.js
@@ -1,21 +1,56 @@
-import axios from 'axios';
-import React,{useEffect, useState} from 'react';
+/* eslint-disable */
+// import axios from 'axios';
+// import {useEffect, useState} from 'react';
 import { useHistory,useParams } from 'react-router-dom/cjs/react-router-dom.min';
-import { Button,Navbar,Container,Nav,NavDropdown,Carousel } from 'react-bootstrap';
+import { Table, Pagination } from 'react-bootstrap';
 import {CSSTransition} from "react-transition-group"
+import './Notice.css'
 
-function Notice(){
-
-  return(
-    <div>
+function Notice() {
+  let active = 2;
+  let items = [];
+  for (let number = 1; number <= 5; number++) {
+    items.push(
+      <Pagination.Item key={number} active={number === active}>
+        {number}
+      </Pagination.Item>,
+    );
+  }
+  return (
+    <div className="mt-3">
       <h1>공지사항입니다.</h1>
-      
-
+      <Table className="my-3" striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>등록일</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+      <Pagination>{items}</Pagination>
     </div>
   )
 }
-
-
-
 
 export default Notice
