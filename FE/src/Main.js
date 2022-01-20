d247c8b899e2123c3bc362b914e67eb387a68d28
jodie9596@gmail.com <jodie9596@gmail.com>
2022-01-20 14:08:33 +0900

    Feat: Main base

--- FE/src/Main.js
+++ FE/src/Main.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React,{useEffect, useState} from 'react';
 import { useHistory,useParams } from 'react-router-dom/cjs/react-router-dom.min';
-import { Button,Navbar,Container,Nav,NavDropdown,Carousel,Row,Col } from 'react-bootstrap';
+import { Button,Navbar,Container,Nav,NavDropdown,Carousel,Row,Col, InputGroup, FormControl } from 'react-bootstrap';
 import {CSSTransition} from "react-transition-group"
 
 function Main(){
@@ -14,15 +14,35 @@ function Main(){
       
       <h1>밖에서 술 드시기 힘드시죠?</h1>
       <p>이런 공간이 있다면 어떨까요...
-...온라인 술자리도 오프라인 술자리 처럼 느낄 수 있는 공간. 소중한 단짝 친구들과 바로 옆에 있는 듯한 느낌을 주는 우리만의 공간. 더 쉽게, 매일 어울리고 즐겁게 술 마실 수 있는 그런 공간 말이에요.</p>
+        온라인 술자리도 오프라인 술자리 처럼 느낄 수 있는 공간. 소중한 단짝 친구들과 바로 옆에 있는 듯한 느낌을 주는 우리만의 공간. 더 쉽게, 매일 어울리고 즐겁게 술 마실 수 있는 그런 공간 말이에요.</p>
     </Col>
     <Col></Col>
   </Row>
+
+  <Row>
+    <Col></Col>
+    <Col xs={3}>
+      <Button variant="warning">방만들기</Button>{' '}
+    </Col>
+    <Col>
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Code : 123456"
+          aria-label="Code : 123456"
+          aria-describedby="basic-addon2"
+        />
+        <Button variant="warning" id="button-addon2">
+          입장하기
+        </Button>
+      </InputGroup>
+    </Col>
+    
+  <Col></Col>
+  </Row>
+
   <Row>
     <Col></Col>
     <Col xs={6}>
-
-      <h1>방개설하기 // 코드입력란</h1>
       <h1>최근 업데이트 내역 삽입</h1>
       <h2>서비스에 대해 설명하는창...</h2>
 
