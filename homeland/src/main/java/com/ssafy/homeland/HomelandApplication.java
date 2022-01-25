3c9c48f0a9ae77926c14bd0fccccc71505d07ac1
박성건 <qs2720@naver.com>
2022-01-25 19:23:21 +0900

    Breaking chage:프로젝트 틀 변경

---
+++ homeland/src/main/java/com/ssafy/homeland/HomelandApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.homeland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomelandApplication.class, args);
+	}
+
+}
