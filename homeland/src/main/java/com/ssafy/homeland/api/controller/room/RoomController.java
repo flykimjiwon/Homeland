eb9f4fd8b2f47e69ea45895e4223003138666c25
rudy0103 <qs2720@naver.com>
2022-02-06 16:41:12 +0900

    Fix: 방참여 관련CrossOrigin

--- homeland/src/main/java/com/ssafy/homeland/api/controller/room/RoomController.java
+++ homeland/src/main/java/com/ssafy/homeland/api/controller/room/RoomController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -23,6 +20,7 @@ import java.net.URL;
 
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/v1/room")
 public class RoomController {
 
