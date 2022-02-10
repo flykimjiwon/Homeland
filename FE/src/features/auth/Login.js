9fe4c4fd8ae0deef0f3c21988f4e80219943971c
이종준 <ljj9316@naver.com>
2022-02-10 11:36:48 +0900

    BugFix: 회원가입 유효성검사

--- FE/src/features/auth/Login.js
+++ FE/src/features/auth/Login.js
@@ -1,10 +1,24 @@
 /* eslint-disable */
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useHistory, Link } from "react-router-dom";
-import { Button, Container, Form } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import "./Login.css";
 import backEndUrl from "../setup/hld_url";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
 
 function Login() {
   const BEUrl = backEndUrl;
@@ -35,7 +49,7 @@ function Login() {
         history.push("/");
       })
       .catch((err) => {
-        if (err.response.status === 500) {
+        if (err.response.status === 404) {
           alert("존재하지 않는 아이디입니다.");
         } else if (err.response.status === 401) {
           alert("비밀번호가 틀렸습니다.");
@@ -44,40 +58,104 @@ function Login() {
   };
 
   return (
-    <div>
-      <h1 className="mt-3">로그인 페이지 입니다.</h1>
-      <Container className="loginForm">
-        <Form>
-          <Form.Group className="mb-3" controlId="FormID">
-            <Form.Label>ID</Form.Label>
-            <Form.Control
-              value={id}
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            로그인
+          </Typography>
+          <Box component="form" onSubmit={onLogin} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              value={id || ""}
               onChange={handleId}
-              type="text"
-              placeholder="ID를 입력하세요."
+              id="id"
+              label="아이디"
+              name="id"
+              autoComplete="id"
+              autoFocus
             />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>비밀번호</Form.Label>
-            <Form.Control
+            <TextField
+              margin="normal"
+              required
+              fullWidth
               value={password}
               onChange={handlePassword}
+              name="password"
+              label="비밀번호"
               type="password"
-              placeholder="비밀번호를 입력하세요."
+              id="password"
+              autoComplete="current-password"
             />
-          </Form.Group>
-          <Form.Group className="d-flex justify-content-center">
-            <Button variant="primary" type="submit" onClick={onLogin}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
               로그인
             </Button>
-          </Form.Group>
-          <Link to="/check-email">
-            <p>비밀번호 찾기</p>
-          </Link>
-        </Form>
+            <div className="d-flex justify-content-between">
+              <div>
+                <Link to="/check-email" variant="body2">
+                  비밀번호 찾기
+                </Link>
+              </div>
+              <div>
+                <Link to="/signup" variant="body2">
+                  회원가입
+                </Link>
+              </div>
+            </div>
+          </Box>
+        </Box>
       </Container>
-    </div>
+    </ThemeProvider>
+
+    // <Container className="loginForm">
+    //   <Form>
+    //     <Form.Group className="mb-3" controlId="FormID">
+    //       <Form.Label>ID</Form.Label>
+    //       <Form.Control
+    //         value={id}
+    //         onChange={handleId}
+    //         type="text"
+    //         placeholder="ID를 입력하세요."
+    //       />
+    //     </Form.Group>
+
+    //     <Form.Group className="mb-3" controlId="formBasicPassword">
+    //       <Form.Label>비밀번호</Form.Label>
+    //       <Form.Control
+    //         value={password}
+    //         onChange={handlePassword}
+    //         type="password"
+    //         placeholder="비밀번호를 입력하세요."
+    //       />
+    //     </Form.Group>
+    //     <Form.Group className="d-flex justify-content-center">
+    //       <Button variant="primary" type="submit" onClick={onLogin}>
+    //         로그인
+    //       </Button>
+    //     </Form.Group>
+    //     <Link to="/check-email">
+    //       <p>비밀번호 찾기</p>
+    //     </Link>
+    //   </Form>
+    // </Container>
   );
 }
 
