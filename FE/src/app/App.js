581694f6b87954dba70583016dd40b02d32ca823
jodie9596@gmail.com <jodie9596@gmail.com>
2022-02-16 13:39:04 +0900

    Design: nav font change

--- FE/src/app/App.js
+++ FE/src/app/App.js
@@ -67,7 +67,7 @@ function App() {
           className="color-nav nav-fix"
           variant="light"
         >
-          <Container>
+          <Container style={{ fontSize: 20, fontWeight: 600 }}>
             <Navbar.Brand as={Link} to="/">
               {/* HomeLanDrink */}
               <img src={Logo} width="80"></img>
@@ -75,10 +75,20 @@ function App() {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link eventKey="link-1" as={Link} to="/notice">
+                <Nav.Link
+                  eventKey="link-1"
+                  as={Link}
+                  to="/notice"
+                  style={{ color: "#353f71" }}
+                >
                   공지사항
                 </Nav.Link>
-                <Nav.Link eventKey="link-2" as={Link} to="/meeting-main">
+                <Nav.Link
+                  eventKey="link-2"
+                  as={Link}
+                  to="/meeting-main"
+                  style={{ color: "#353f71" }}
+                >
                   참가
                 </Nav.Link>
               </Nav>
@@ -90,19 +100,35 @@ function App() {
                     eventKey="link-4"
                     as={Link}
                     to="/logout"
+                    style={{ color: "#353f71" }}
                   >
                     로그아웃
                   </Nav.Link>
-                  <Nav.Link eventKey="link-6" as={Link} to="/mypage">
+                  <Nav.Link
+                    eventKey="link-6"
+                    as={Link}
+                    to="/mypage"
+                    style={{ color: "#353f71" }}
+                  >
                     마이페이지
                   </Nav.Link>
                 </Nav>
               ) : (
                 <Nav>
-                  <Nav.Link eventKey="link-3" as={Link} to="/login">
+                  <Nav.Link
+                    eventKey="link-3"
+                    as={Link}
+                    to="/login"
+                    style={{ color: "#353f71" }}
+                  >
                     로그인
                   </Nav.Link>
-                  <Nav.Link eventKey="link-5" as={Link} to="/signup">
+                  <Nav.Link
+                    eventKey="link-5"
+                    as={Link}
+                    to="/signup"
+                    style={{ color: "#353f71" }}
+                  >
                     회원가입
                   </Nav.Link>
                 </Nav>
